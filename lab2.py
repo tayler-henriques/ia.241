700cc21e5d7a809790a350b7e08f26260a3fc8d4
EC2 Default User <ec2-user@ip-172-31-50-209.ec2.internal>
2021-01-28 19:26:27 +0000

    my lab2

---
+++ lab2.py
@@ -0,0 +1,26 @@
+"lab 2"
+
+my_name = "Tom"
+print(my_name)
+
+my_id= 123
+print(my_id)
+
+#123=my_id
+
+my_id=your_id=123
+print(my_id)
+print(your_id)
+
+my_id_str= '123'
+print(type(my_id_str))
+print(my_id_str)
+
+#print(my_name + my_id)
+
+print(my_name + my_id_str)
+
+print(my_name*3)
+
+print('Hello world. This is my first pythong string.' .split('.'))
+#message='Tom's id is 213'
